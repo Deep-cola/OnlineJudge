fbe66a31c359f4b4190d68541e5246a2eaa4f82e
Cola <2947595536@qq.com>
2020-11-20 21:12:42 +0800

    for OJ of heap

---
+++ LeetCode/Solution1046.java
@@ -0,0 +1,50 @@
+package LeetCode;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * @description: 最后一块石头的重量
+ * 有一堆石头，每块石头的重量都是正整数。
+ * 每一回合，从中选出两块 最重的 石头，然后将它们一起粉碎。假设石头的重量分别为 x 和 y，且 x <= y。那么粉碎的可能结果如下：
+ *      如果 x == y，那么两块石头都会被完全粉碎；
+ *      如果 x != y，那么重量为 x 的石头将会完全粉碎，而重量为 y 的石头新重量为 y-x。
+ * 最后，最多只会剩下一块石头。返回此石头的重量。如果没有石头剩下，就返回 0。
+ * @author: Deepcola
+ * @time: 2020/11/20 16:00
+ */
+public class Solution1046 {
+
+    /**
+     * 从中取出两块最重的石头, 只需要维护一个大根堆, 两次弹出堆顶元素，
+     * 然后将两块石头碰撞之后的新石头继续放入，维护大根堆，直至剩下一块石头或者堆为空
+     */
+    public int lastStoneWeight(int[] stones) {
+        // 建立大根堆
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        // 石头进堆 -> 维护大根堆
+        for (int stone: stones) {
+            priorityQueue.offer(stone);
+        }
+        // 石头碰撞
+        while (priorityQueue.size() > 1) {
+            priorityQueue.offer(priorityQueue.poll() - priorityQueue.poll());
+        }
+        // 还有一块石头
+        if (!priorityQueue.isEmpty()) {
+            return priorityQueue.poll();
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Solution1046 solution = new Solution1046();
+        int[] stones = {2,7,4,1,8,1};
+        System.out.println(solution.lastStoneWeight(stones));
+    }
+}
---
+++ LeetCode/Solution662.java
@@ -0,0 +1,71 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @description: 二叉树最大宽度
+ * 给定一个二叉树，编写一个函数来获取这个树的最大宽度。树的宽度是所有层中的最大宽度。这个二叉树与满二叉树（full binary tree）结构相同，但一些节点为空。
+ * 每一层的宽度被定义为两个端点（该层最左和最右的非空节点，两端点间的null节点也计入长度）之间的长度
+ * @author: Deepcola
+ * @time: 2020/11/20 7:57
+ */
+public class Solution662 {
+    /**
+     * Definition for a binary tree node
+     */
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    /**
+     * 二叉树的最大宽度
+     * 1.空树是宽度为 0；
+     * 2.非空树时宽度 maxWidth 最小为 1
+     * 3.记录每一个非空结点的下标，用每一层的 最优结点-最左结点+1 就是该层的宽度
+     */
+    public int widthOfBinaryTree(TreeNode root) {
+        // 空树
+        if (root == null) return 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+        // 存放每个不为空结点下标
+        List<Integer> list = new ArrayList<>();
+        // 根节点入队，记根结点下标为 0
+        queue.offer(root);
+        list.add(0);
+        // 最小宽度为 1
+        int maxWidth = 1;
+        while (!queue.isEmpty()) {
+            // 记录每层装入的结点个数
+            int count = queue.size();
+            while (count-- > 0) {
+                TreeNode temp = queue.poll();
+                // 记录当前移除结点的下标用以计算左右孩子的下标
+                int index = list.remove(0);
+                // 非空孩子结点入队
+                // 计算该非空孩子结点的下标, 添加到 list
+                if (temp.left != null) {
+                    queue.offer(temp.left);
+                    list.add( 2 * index + 1);
+                }
+                if (temp.right != null) {
+                    queue.offer(temp.right);
+                    list.add(2 * index + 2);
+                }
+            }
+            // 对比该层的宽度和前面的最大宽度
+            if (list.size() > 1) {
+                int width = list.get(list.size() - 1) - list.get(0) + 1;
+                maxWidth = Math.max(width, maxWidth);
+            }
+        }
+        return maxWidth;
+    }
+}
