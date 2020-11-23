02b32f70bf12585f104e961f1501351f36543c2e
Cola <2947595536@qq.com>
2020-11-23 23:29:02 +0800

    for LeetCode

---
+++ LeetCode/Solution206.java
@@ -0,0 +1,51 @@
+package LeetCode;
+
+/**
+ * @description: 反转链表
+ * 反转一个单链表。
+ *
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * @author: Deepcola
+ * @time: 2020/11/23 16:59
+ */
+public class Solution206 {
+
+    /**
+     * Definition for singly-linked list
+     */
+    class ListNode {
+        int val;
+        ListNode next;
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    /**
+     * 1.判断链表是否为空
+     * 2.prev 标记 cur 的前驱结点
+     * 3.curNext 指向原链表 cur 的 next 结点
+     * 4.循环反转链表知道 cur = null
+     * 5.此时, prev 就是新的头结点
+     */
+    public ListNode reverseList(ListNode head) {
+        if (head == null) return null;
+        ListNode cur = head;
+        // 标记 cur 结点的前驱结点, 也是反转链表后的 cur.next
+        ListNode prev = null;
+        while (cur != null) {
+            // 指向未反转前的 cur.next,
+            // 在 cur 发生反转后, next域 会发生改变,所以需要一个结点来标记它原本的 next 结点
+            ListNode curNext = cur.next;
+            // 反转
+            cur.next = prev;
+            prev = cur;
+            // 按照原本的连接遍历
+            cur = curNext;
+        }
+        // 此时 cur 结点为 null, prev 就是原本链表的最后一个结点, 反转链表的头结点
+        head = prev;
+        return head;
+    }
+}
---
+++ LeetCode/Solution21.java
@@ -0,0 +1,62 @@
+package LeetCode;
+
+/**
+ * @description: 合并两个有序链表
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ * @author: Deepcola
+ * @time: 2020/11/23 18:09
+ */
+public class Solution21 {
+
+    /**
+     * Definition for singly-linked list
+     */
+    class ListNode {
+        int val;
+        ListNode next;
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    /**
+     * 1.特殊情况:
+     *          l1 l2 都为空 -> null
+     *          l1 为空 -> l2
+     *          l2 为空 -> l1
+     * 2.定义一个新的 newHead, 用来表示新链表
+     * 3.遍历比较 两个链表, 将较小的值依次插入新链表, 直到有一个链表遍历完
+     * 4.将另一个链表全部插入到新链表后面
+     * 5.新链表头结点是自己定义的, 所以下一个才是合并的头结点
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) return null;
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        ListNode newHead = new ListNode(0);
+        // 用于标记新链表的末尾位置
+        ListNode temp = newHead;
+        // 遍历比较两个链表的值, 从小到大加入新链表, 知道一个链表被添加完
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                temp.next = l1;
+                temp = temp.next;
+                l1 = l1.next;
+            }else {
+                temp.next = l2;
+                temp = temp.next;
+                l2 = l2.next;
+            }
+        }
+        // 此时, 有一个链表被遍历完, 将另一个链表加在新链表后面即可
+        if (l1 != null) {
+            temp.next = l1;
+        }else {
+            temp.next = l2;
+        }
+        return newHead.next;
+    }
+}
