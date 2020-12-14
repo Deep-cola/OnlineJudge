fa314c62e66495217e0b5a983052ac4d3ddcafa6
Cola <2947595536@qq.com>
2020-12-15 00:22:02 +0800

    for daily questions

--- NewCode/Bodd.java
+++ NewCode/Bodd.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 /**
  * @description:
+ * 输入描述:     输入在一行中给出正方形边长N（3<=N<=20）和组成正方形边的某种字符C，间隔一个空格。
+ * 输出描述:     输出由给定字符C画出的正方形。但是注意到行间距比列间距大，所以为了让结果看上去更像正方形，我们输出的行数实际上是列数的50%（四舍五入取整）。
+ * 输入   10 a
+ * 输出 aaaaaaaaaa
+ *     a        a
+ *     a        a
+ *     a        a
+ *     aaaaaaaaaa
  * @author: Deepcola
  * @time: 2020/12/7 7:09
  */
