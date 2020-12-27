9d72ee6ec4a6805438b9bff3eaaae4c1c3acaafc
Cola <2947595536@qq.com>
2020-12-27 13:07:19 +0800

    for exercises

--- LeetCode/Solution205.java
+++ LeetCode/Solution205.java
@@ -1,16 +1,57 @@
 package leetcode;
 
+import javafx.scene.Parent;
+
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @description: 同构字符串
  * 给定两个字符串 s 和 t，判断它们是否是同构的。
  * 如果 s 中的字符可以被替换得到 t ，那么这两个字符串是同构的。
  * 所有出现的字符都必须用另一个字符替换，同时保留字符的顺序。两个字符不能映射到同一个字符上，但字符可以映射自己本身。
+ *      你可以假设 s 和 t 具有相同的长度。
  * @author: Deepcola
  * @time: 2020/11/22 15:40
  */
 public class Solution205 {
+
+    /**
+     * 数组统计: 记录任意字符上次出现的下标, 用于比较
+     */
+    public boolean isIsomorphic(String s, String t) {
+        char[] charS = s.toCharArray();
+        char[] charT = t.toCharArray();
+        int[] preIndexOfS = new int[256];
+        int[] preIndexOdT = new int[256];
+        for (int i = 0; i < s.length(); i++) {
+            if (preIndexOdT[charT[i]] != preIndexOfS[charS[i]]) {
+                return false;
+            }
+            preIndexOdT[charT[i]] = i + 1;
+            preIndexOfS[charS[i]] = i + 1;
+        }
+        return true;
+    }
+
+    /**
+     * 双射: 对应用两个哈希表
+     */
+    /*public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> s2t = new HashMap<>();
+        Map<Character, Character> t2s = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char chS = s.charAt(i);
+            char chT = t.charAt(i);
+            // 不能达成双射
+            if (s2t.containsKey(chS) && s2t.get(chS) != chT || t2s.containsKey(chT) && t2s.get(chT) != chS) {
+                return false;
+            }
+            s2t.put(chS, chT);
+            t2s.put(chT, chS);
+        }
+        return true;
+    }*/
 
     /**
      * 判断某一个字母第一次出现的位置的下标是否相同
@@ -32,7 +73,7 @@ public class Solution205 {
      *      都相同但是下标不一致 -> false;
      *      都相同下标一致 -> true
      */
-    public boolean isIsomorphic(String s, String t) {
+    /*public boolean isIsomorphic(String s, String t) {
         if (s.length() != t.length()) return false;
         HashMap<Character, Character> map = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
@@ -56,6 +97,6 @@ public class Solution205 {
             }
         }
         return true;
-    }
+    }*/
 
 }
