9194c366105651da10bbd90c1a9a6d9e379160a0
Cola <2947595536@qq.com>
2021-01-02 14:18:13 +0800

    for exercises

--- src/leetcode/Solution239.java
+++ src/leetcode/Solution239.java
@@ -47,15 +47,49 @@ public class Solution239 {
 
     /**
      * 分块 + 预处理:
-     *      数组分块: k 个一组, 最后一组元素个数可能不到 k 个
-     *       对于 nums[i] ~ nums[i+k-1] 窗体中的最大值:
-     *          当 i 是 k 的倍数, 窗体对应一组，最大值就是该分组中的最大值;
-     *          当 i 不是 k 的倍数, 窗体就会跨越两个分组 -> 假设 j 是 k 的倍数 (i <= k <= i+k-1)
-     *                          -> 两个分组是 nums[i] ~ nums[j-1]; nums[j] ~ nums[i+k-1]
-     *
+     *      1.数组分块: k 个一组, 最后一组元素个数可能不到 k 个
+     *        对于 nums[i] ~ nums[i+k-1] 窗体中的最大值:
+     *          (1)当 i 是 k 的倍数, 窗体对应一组，最大值就是该分组中的最大值;
+     *          (2)当 i 不是 k 的倍数, 窗体就会跨越两个分组 -> 假设 j 是 k 的倍数 (i <= k <= i+k-1)
+     *                          -> 那么窗体跨越了第一个分组后缀 nums[i] ~ nums[j-1]; 第二个分组前缀 nums[j] ~ nums[i+k-1]
+     *      2.预处理:使用 suffixMax[i] 表示下标 i 对应的分组中, 以 i 开始的后缀最大值; 使用 prefixMax[i] 表示下标 i 对应的分组中, 以 i 结束的前缀最大值;
+     *        suffixMax[i] = nums[i]    i+1 是 k 的倍数
+     *                     = max{suffixMax[i+1], nums[i]}   i+1 不是 k 的倍数
+     *        prefixMax[i] = nums[i]    i 是 k 的倍数
+     *                     = max{prefixMax[i-1], nums[i]}   i 不是 k 的倍数
+     *      3.对于 nums[i] ~ nums[i+k-1] 窗体中的最大值
+     *          -> 当 i 是 k 的倍数, 窗体恰好对应一部分分组, 最大值为 suffixMax[i] 或者 prefixMax[i+k-1] 均可
+     *          -> 当 i 不是 k 的倍数, 跨越两个分组, 最大值为 max{suffixMax[i], prefixMax[i+k-1]}
      */
     public int[] maxSlidingWindow(int[] nums, int k) {
-
+        int n = nums.length;
+        int[] prefixMax = new int[n];
+        int[] suffixMax = new int[n];
+        // 计算prefixMax
+        for (int i = 0; i < n; i++) {
+            // i 是 k 的倍数
+            if (i % k == 0) {
+                prefixMax[i] = nums[i];
+            }else {
+                // i 不是 k 的倍数
+                prefixMax[i] = Math.max(prefixMax[i - 1], nums[i]);
+            }
+        }
+        // 计算 suffixMax
+        for (int i = n - 1; i >= 0; i--) {
+            // i+1 是 i 的倍数 或者说是最后一个数
+            if ((i + 1) % k == 0 || i == n - 1) {
+                suffixMax[i] = nums[i];
+            }else {
+                suffixMax[i] = Math.max(suffixMax[i + 1], nums[i]);
+            }
+        }
+        // 计算结果
+        int[] result = new int[n - k + 1];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = Math.max(suffixMax[i], prefixMax[i + k - 1]);
+        }
+        return result;
     }
 
 
