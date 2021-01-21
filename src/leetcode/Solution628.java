8c901b105c05a189fa9d424e9e0b1a91170877c3
Cola <2947595536@qq.com>
2021-01-21 22:01:47 +0800

    for exercises

--- src/leetcode/Solution628.java
+++ src/leetcode/Solution628.java
@@ -28,9 +28,48 @@ public class Solution628 {
      * 2.全是负数 : 最大三个数乘积
      * 3.有正有负 : num[0] * num[1] * nums[n - 1];
      */
-    public int maximumProduct(int[] nums) {
+    /*public int maximumProduct(int[] nums) {
         Arrays.sort(nums);
         return Math.max(nums[0] * nums[1] * nums[nums.length - 1],
                 nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3]);
+    }*/
+
+    /**
+     * 只需要找出排序之后的前两个数字和最后三个数字即可
+     */
+    public int maximumProduct(int[] nums) {
+        // 最小的 和 第二小的
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        // 最大的 第二大的 第三大的
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        int max3 = Integer.MIN_VALUE;
+
+        for (int x: nums) {
+            if (x < min1) {
+                // 最小的
+                min2 = min1;
+                min1 = x;
+            }else if (x < min2) {
+                // 大于最小的, 小于第二小的
+                min2 = x;
+            }
+
+            if (x > max1) {
+                // 最大的
+                max3 = max2;
+                max2 = max1;
+                max1 = x;
+            }else if (x > max2) {
+                // 第二大的
+                max3 = max2;
+                max2 = x;
+            }else if (x > max3) {
+                // 第三大的
+                max3 = x;
+            }
+        }
+        return Math.max(min1 * min2 * max1, max1 * max2 * max3);
     }
 }
