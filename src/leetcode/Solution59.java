e018838c95fab76a0325f82a687dd36915e4eb48
Cola <2947595536@qq.com>
2021-03-16 14:03:28 +0800

    for exercises

---
+++ src/leetcode/Solution59.java
@@ -0,0 +1,78 @@
+package leetcode;
+
+/**
+ * @description:  螺旋矩阵Ⅱ
+ * 给你一个正整数 n ，生成一个包含 1 到 n2 所有元素，且元素按顺时针顺序螺旋排列的 n x n 正方形矩阵 matrix 。
+ *
+ * 示例 1：
+ *
+ * 输入：n = 3
+ * 输出：[[1,2,3],[8,9,4],[7,6,5]]
+ * 示例 2：
+ *
+ * 输入：n = 1
+ * 输出：[[1]]
+ *
+ * 提示：
+ *
+ * 1 <= n <= 20
+ * @author: Deepcola
+ * @time: 2021/3/16 14:00
+ */
+public class Solution59 {
+
+    public int[][] generateMatrix(int n) {
+        int num = 1;
+        int[][] matrix = new int[n][n];
+        int left = 0, right = n - 1, top = 0, bottom = n - 1;
+        while (left <= right && top <= bottom) {
+            for (int column = left; column <= right; column++) {
+                matrix[top][column] = num;
+                num++;
+            }
+            for (int row = top + 1; row <= bottom; row++) {
+                matrix[row][right] = num;
+                num++;
+            }
+            if (left < right && top < bottom) {
+                for (int column = right - 1; column > left; column--) {
+                    matrix[bottom][column] = num;
+                    num++;
+                }
+                for (int row = bottom; row > top; row--) {
+                    matrix[row][left] = num;
+                    num++;
+                }
+            }
+            left++;
+            right--;
+            top++;
+            bottom--;
+        }
+        return matrix;
+    }
+
+    /*public int[][] generateMatrix(int n) {
+        int[][] res = new int[n][n];
+        int up = 0, down = n - 1, left = 0, right = n - 1, index = 1;
+        while(index <= n * n){
+            for(int i = left; i <= right; i++){
+                res[up][i] = index++;
+            }
+            up++;
+            for(int i = up; i <= down; i++){
+                res[i][right] = index++;
+            }
+            right--;
+            for(int i = right; i >= left; i--){
+                res[down][i] = index++;
+            }
+            down--;
+            for(int i = down; i >= up; i--){
+                res[i][left] = index++;
+            }
+            left++;
+        }
+        return res;
+    }*/
+}
