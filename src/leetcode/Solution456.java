c26a6f7ec958ca67f4170c6123502bd946c49a7e
Cola <2947595536@qq.com>
2021-03-25 17:21:17 +0800

    for exercises

--- src/leetcode/Solution456.java
+++ src/leetcode/Solution456.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.TreeMap;
 
 /**
@@ -74,4 +76,28 @@ public class Solution456 {
         }
         return false;
     }
+
+    /**
+     * 枚举 1
+     */
+    /*public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+        Deque<Integer> candidateK = new LinkedList<Integer>();
+        candidateK.push(nums[n - 1]);
+        int maxK = Integer.MIN_VALUE;
+
+        for (int i = n - 2; i >= 0; --i) {
+            if (nums[i] < maxK) {
+                return true;
+            }
+            while (!candidateK.isEmpty() && nums[i] > candidateK.peek()) {
+                maxK = candidateK.pop();
+            }
+            if (nums[i] > maxK) {
+                candidateK.push(nums[i]);
+            }
+        }
+
+        return false;
+    }*/
 }
