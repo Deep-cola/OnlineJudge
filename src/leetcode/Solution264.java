598fcd30d71fb6d851f7553a720549ce4783c83c
Cola <2947595536@qq.com>
2021-04-13 17:46:43 +0800

    for exercises

--- src/leetcode/Solution264.java
+++ src/leetcode/Solution264.java
@@ -1,5 +1,9 @@
 package leetcode;
 
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
 /**
  * @description: 丑数 Ⅱ
  * 给你一个整数 n ，请你找出并返回第 n 个 丑数 。
@@ -50,4 +54,28 @@ public class Solution264 {
         }
         return dp[n];
     }
+
+
+    /**
+     * 最小堆
+     */
+    /*public int nthUglyNumber(int n) {
+        int[] factors = {2, 3, 5};
+        Set<Long> seen = new HashSet<Long>();
+        PriorityQueue<Long> heap = new PriorityQueue<Long>();
+        seen.add(1L);
+        heap.offer(1L);
+        int ugly = 0;
+        for (int i = 0; i < n; i++) {
+            long curr = heap.poll();
+            ugly = (int) curr;
+            for (int factor : factors) {
+                long next = curr * factor;
+                if (seen.add(next)) {
+                    heap.offer(next);
+                }
+            }
+        }
+        return ugly;
+    }*/
 }
