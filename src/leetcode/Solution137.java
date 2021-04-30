848297fa98f8227461e53f94f78ada009253456e
Cola <2947595536@qq.com>
2021-04-30 15:28:51 +0800

    for exercises

---
+++ src/leetcode/Solution137.java
@@ -0,0 +1,50 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description: 只出现一次的数字 Ⅱ
+ * 给你一个整数数组 nums ，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次 。请你找出并返回那个只出现了一次的元素。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [2,2,3,2]
+ * 输出：3
+ * 示例 2：
+ *
+ * 输入：nums = [0,1,0,1,0,1,99]
+ * 输出：99
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 3 * 104
+ * -231 <= nums[i] <= 231 - 1
+ * nums 中，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次
+ * @author: Deepcola
+ * @time: 2021/4/30 15:27
+ */
+public class Solution137 {
+
+    /**
+     * 哈希表
+     */
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> freq = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
+        int ans = 0;
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            int num = entry.getKey(), occ = entry.getValue();
+            if (occ == 1) {
+                ans = num;
+                break;
+            }
+        }
+        return ans;
+    }
+}
