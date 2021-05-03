806242bd616d1ba381bed4ade77e8ee4bce90196
Cola <2947595536@qq.com>
2021-05-03 18:45:55 +0800

    update

--- src/leetcode/Solution7.java
+++ src/leetcode/Solution7.java
@@ -36,7 +36,7 @@ package leetcode;
 public class Solution7 {
 
 
-    public int reverse(int x) {
+    /*public int reverse(int x) {
         int rev = 0;
         while (x != 0) {
             int pop = x % 10;
@@ -44,6 +44,22 @@ public class Solution7 {
             if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
             if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
             rev = rev * 10 + pop;
+        }
+        return rev;
+    }*/
+
+    /**
+     * 数学
+     */
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            if (rev < Integer.MIN_VALUE / 10 || rev > Integer.MAX_VALUE / 10) {
+                return 0;
+            }
+            int digit = x % 10;
+            x /= 10;
+            rev = rev * 10 + digit;
         }
         return rev;
     }
