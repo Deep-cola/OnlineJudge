8b14274fc9af571b616d7a0fea759641dc63ac45
Cola <2947595536@qq.com>
2021-05-10 20:42:51 +0800

    for、 exercises

--- src/leetcode/Solution1723.java
+++ src/leetcode/Solution1723.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * @description: 完成所有工作的最短时间
  * 给你一个整数数组 jobs ，其中 jobs[i] 是完成第 i 项工作要花费的时间。
@@ -35,9 +37,62 @@ package leetcode;
 public class Solution1723 {
 
     /**
-     * 动态规划 + 状态压缩
+     * 二分查找 + 回溯 + 剪枝
      */
     public int minimumTimeRequired(int[] jobs, int k) {
+        Arrays.sort(jobs);
+        int low = 0, high = jobs.length - 1;
+        while (low < high) {
+            int temp = jobs[low];
+            jobs[low] = jobs[high];
+            jobs[high] = temp;
+            low++;
+            high--;
+        }
+        int l = jobs[0], r = Arrays.stream(jobs).sum();
+        while (l < r) {
+            int mid = (l + r) >> 1;
+            if (check(jobs, k, mid)) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+
+    public boolean check(int[] jobs, int k, int limit) {
+        int[] workloads = new int[k];
+        return backtrack(jobs, workloads, 0, limit);
+    }
+
+    public boolean backtrack(int[] jobs, int[] workloads, int i, int limit) {
+        if (i >= jobs.length) {
+            return true;
+        }
+        int cur = jobs[i];
+        for (int j = 0; j < workloads.length; ++j) {
+            if (workloads[j] + cur <= limit) {
+                workloads[j] += cur;
+                if (backtrack(jobs, workloads, i + 1, limit)) {
+                    return true;
+                }
+                workloads[j] -= cur;
+            }
+            // 如果当前工人未被分配工作，那么下一个工人也必然未被分配工作
+            // 或者当前工作恰能使该工人的工作量达到了上限
+            // 这两种情况下我们无需尝试继续分配工作
+            if (workloads[j] == 0 || workloads[j] + cur == limit) {
+                break;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 动态规划 + 状态压缩
+     */
+    /*public int minimumTimeRequired(int[] jobs, int k) {
         int n = jobs.length;
         int[] sum = new int[1 << n];
         for (int i = 1; i < (1 << n); i++) {
@@ -60,5 +115,5 @@ public class Solution1723 {
             }
         }
         return dp[k - 1][(1 << n) - 1];
-    }
+    }*/
 }
